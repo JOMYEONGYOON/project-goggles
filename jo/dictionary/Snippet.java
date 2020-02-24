3bd51f539af12ffc412619519c94e946690d5620
JOMYEONGYOON <61299324+JOMYEONGYOON@users.noreply.github.com>
2020-02-24 15:15:45 +0900

    sql

---
+++ jo/dictionary/Snippet.java
@@ -0,0 +1,16 @@
+package dictionary;
+
+public class Snippet {
+	public static void main(String[] args) {
+//		member.setBirth(birth);
+//		member.setEmail(email);
+//		member.setEtc(etc);
+//		member.setGender(gender);
+//		member.setId(id);
+//		member.setNo(no);
+//		member.setPassword(password);
+//		member.setPhone(phone);
+//		member.setName(name);
+
+	}
+}
