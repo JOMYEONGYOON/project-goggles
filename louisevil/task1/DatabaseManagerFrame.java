bbe519f9334e4fab8e4917e0dc08b577e07af60b
choilib <louis.develog@gmail.com>
2020-02-29 04:39:26 +0900

    Update DatabaseManagerFrame.java

--- louisevil/task1/DatabaseManagerFrame.java
+++ louisevil/task1/DatabaseManagerFrame.java
@@ -2,6 +2,7 @@ package task1;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.BufferedReader;
@@ -10,58 +11,63 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.tree.TreePath;
 import javax.swing.tree.TreeSelectionModel;
-import javax.swing.JButton;
-import javax.swing.SwingConstants;
-import java.awt.GridLayout;
 
 public class DatabaseManagerFrame extends JFrame {
+	private JPanel southPanel;
+	private JPanel panel;
+	private JScrollPane textAreaScrollPane;
+	private JPanel northPanel;
+	private JScrollPane scrollPane;
+
 	public DatabaseManagerFrame() {
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().setFont(new Font("Dialog", Font.PLAIN, 10));
 		setSize(800, 500);
 		getContentPane().setLayout(null);
-		JPanel panel = new JPanel();
+		panel = new JPanel();
 		getContentPane().setLayout(new BorderLayout(0, 0));
 		panel.setLayout(new BorderLayout(0, 0));
 		JTextArea textArea = new JTextArea();
-		JScrollPane textAreaScrollPane = new JScrollPane(textArea);
+		textAreaScrollPane = new JScrollPane(textArea);
 		textArea.setEditable(false);
 		panel.add(textAreaScrollPane, BorderLayout.CENTER);
 		getContentPane().add(panel);
 
-		JScrollPane scrollPane = new JScrollPane();
+		scrollPane = new JScrollPane();
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		panel.add(scrollPane, BorderLayout.WEST);
 
 		FileJTree sqlTree = new FileJTree("resources\\sql");
 		scrollPane.setViewportView(sqlTree);
 		System.out.println(sqlTree);
-		
-		JPanel northPanel = new JPanel();
+
+		northPanel = new JPanel();
 		panel.add(northPanel, BorderLayout.NORTH);
 		northPanel.setLayout(new GridLayout(1, 2, 0, 0));
-		
+
 		JButton runButton = new JButton("Run");
 		northPanel.add(runButton);
-		
-		JButton btnNewButton_1 = new JButton("Commit");
-		northPanel.add(btnNewButton_1);
-		
-		JButton btnNewButton = new JButton("Rollback");
-		northPanel.add(btnNewButton);
-		
-		JButton btnNewButton_2 = new JButton("Exit");
-		northPanel.add(btnNewButton_2);
-		
-		JPanel southPanel = new JPanel();
+
+		JButton tableButton = new JButton("Table");
+		northPanel.add(tableButton);
+
+		JLabel emptyLabel = new JLabel("");
+		northPanel.add(emptyLabel);
+
+		JButton exitButton = new JButton("Exit");
+		northPanel.add(exitButton);
+
+		southPanel = new JPanel();
 		panel.add(southPanel, BorderLayout.SOUTH);
 		southPanel.setLayout(new GridLayout(0, 1, 0, 0));
 		sqlTree.addMouseListener(new MouseAdapter() {
@@ -72,7 +78,14 @@ public class DatabaseManagerFrame extends JFrame {
 				if (clickCount == 1) {
 					TreeSelectionModel treeModel = sqlTree.getSelectionModel();
 					TreePath treePath = treeModel.getLeadSelectionPath();
+					System.out.println(treePath);
+					if (treePath.toString().equals("[resources\\sql, insert, word.sql]")) {
+						tableButton.setEnabled(true);
+					} else {
+						tableButton.setEnabled(false);
+					}
 					String path = sqlTree.getPath(treePath);
+					System.out.println(path);
 					StringBuilder sb = new StringBuilder(new String(""));
 					File f = null;
 					f = new File(path);
@@ -93,10 +106,13 @@ public class DatabaseManagerFrame extends JFrame {
 							e.printStackTrace();
 						}
 
-						textArea.setText(sb.toString());
 					} else {
-						
+						String [] fileNames = f.list();
+						for (String s : fileNames) {
+							sb.append(s+"\n");
+						}
 					}
+					textArea.setText(sb.toString());
 
 				}
 			}
@@ -108,5 +124,25 @@ public class DatabaseManagerFrame extends JFrame {
 
 	public static void main(String[] args) {
 		new DatabaseManagerFrame();
+	}
+
+	protected JPanel getSouthPanel() {
+		return southPanel;
+	}
+
+	protected JPanel getPanel() {
+		return panel;
+	}
+
+	protected JScrollPane getTextAreaScrollPane() {
+		return textAreaScrollPane;
+	}
+
+	protected JPanel getNorthPanel() {
+		return northPanel;
+	}
+
+	protected JScrollPane getScrollPane() {
+		return scrollPane;
 	}
 }
