a3b2eb2451d3fd2ccc7422a4562279d5c4701e3d
choilib <louis.develog@gmail.com>
2020-02-29 23:50:56 +0900

    데이터 입력 샘플 추가
    난방, 석탄, 퇴직연금, 환경무역 등등

--- louisevil/task2/MainTest.java
+++ louisevil/task2/MainTest.java
@@ -2,14 +2,12 @@ package task2;
 
 import java.awt.EventQueue;
 
-import swing.MainFrame;
-
 public class MainTest {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					MainFrame window = new MainFrame();
+					MainFrameTest window = new MainFrameTest();
 					window.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
