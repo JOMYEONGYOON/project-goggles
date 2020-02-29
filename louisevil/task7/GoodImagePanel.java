74a18626c4131bfce08c24b974a8c775a74149ff
choilib <louis.develog@gmail.com>
2020-03-01 01:46:17 +0900

    GoodImagePanel

---
+++ louisevil/task7/GoodImagePanel.java
@@ -0,0 +1,34 @@
+package task7;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class GoodImagePanel extends JPanel {
+	private Image bgMainImage = null;
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		g.drawImage(bgMainImage, 0, 0,600,400, null);
+	}
+
+	public Dimension getDimension() {
+		return new Dimension();
+	}
+
+	public GoodImagePanel(String path) {
+		try {
+			this.bgMainImage = ImageIO.read(new File(path));
+			setSize(new Dimension(bgMainImage.getWidth(null), bgMainImage.getHeight(null)));
+			setPreferredSize(new Dimension(600,400));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
+
+	}
+}
