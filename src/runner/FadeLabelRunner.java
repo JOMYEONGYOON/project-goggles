3f362d96b26ed25a440c5ee3cf56e3d3ac6ded39
louisevil <louis.develog@gmail.com>
2020-03-08 00:39:11 +0900

    Update FadeLabelRunner.java

--- src/runner/FadeLabelRunner.java
+++ src/runner/FadeLabelRunner.java
@@ -32,14 +32,13 @@ public class FadeLabelRunner extends Thread {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
+
 		while (count > 0) {
-//			System.out.println(count);
 			Color color = new Color(1.0F, 1.0F, 1.0F, count / 255.0F);
 			successLabel.setForeground(color);
 			try {
 				Thread.sleep(2);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			count--;
